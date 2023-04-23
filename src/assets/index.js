79b18c4bbb2a9a91b5388a727330cfb12ccc4840
Ahmed Elfarra <ahmed.e.alfarra@gmail.com>
2023-04-24 01:42:30 +0300

    commit

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import linkIcon from './link.svg';
+import loader from './loader.svg';
+import copy from './copy.svg';
+import logo from './logo.svg';
+import tick from './tick.svg';
+import enter from './enter.svg';
+
+export { linkIcon, loader, copy, logo, tick, enter };
